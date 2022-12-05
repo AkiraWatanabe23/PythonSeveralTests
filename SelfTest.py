53a6ee16a6a3d4549eda0c24c7d4c68de90a7a09
Akira Watanabe <a0323w@gmail.com>
2022-12-05 16:48:04 +0900

    色々テスト中

--- SelfTest.py
+++ SelfTest.py
@@ -1,4 +1,4 @@
-#Pythonの「self」について記述
+#Python「self」について記述
 #https://www.sejuku.net/blog/64106
 #https://prograshi.com/language/python/py-self-in-class/
 
@@ -10,9 +10,9 @@ instance = TestFirst()
 instance.method()
 """
 実行結果 -> Hello!!
-"""
-#self ... インスタンス自身を示すもの
-"""
+
+self ... インスタンス自身を示すもの
+
 もし、class内で定義したメソッドに引数を1つも渡さなかった場合(使う、使わないに関わらず)
 「TypeError: クラス名.メソッド名() takes 0 positional arguments but 1 was given」
 (メソッドには1つの引数が必須ですが、0個しか渡されていません)というエラーが出る
@@ -34,13 +34,14 @@ class TestSecond():
 test = TestSecond("Good", "Morning!")
 print(test.strA)
 print(test.strB)
+
 """
 実行結果 -> Good (\n) Morning!
-"""
 
-#上記のようにインスタンス(今回は「test」)を生成する時に引数を渡すことで、
-#selfを使ってインスタンス変数として代入することができる
-#※呼び出す側は引数として値を入れない
+上記のようにインスタンス(今回は「test」)を生成する時に引数を渡すことで、
+selfを使ってインスタンス変数として代入することができる
+※呼び出す側は引数として値を入れない
+"""
 
 #使い方 2 :クラス変数として参照する
 #以下のように、クラス変数として別のメソッドで使うことができる
@@ -88,12 +89,11 @@ class Warns():
 test = Warns()
 """
 実行結果 -> 1: Hello Python (\n) 2: Hello World! (\n) 3: Hello World!
-"""
 
-#1 ... self.strA == クラス変数「strA」であるため、OK
-#2 ... self.strAがインスタンス変数として扱われ、代入されているので、OK
-#3 ... pythonの仕様として、クラス変数もインスタンス変数も「self.変数名」の形で参照できるが、
-"""
+1 ... self.strA == クラス変数「strA」であるため、OK
+2 ... self.strAがインスタンス変数として扱われ、代入されているので、OK
+3 ... pythonの仕様として、クラス変数もインスタンス変数も「self.変数名」の形で参照できるが
+
 クラス変数にもインスタンス変数にも値がある場合、インスタンス変数を優先して参照する
 (3 の場合は、コンストラクタ内でstrA(クラス変数)を"Hello python"に変更し、出力しようとしたが
  この時点でクラス変数「strA」とインスタンス変数「self.strA」の両方に値が存在するため、インスタンス変数「self.strA」が優先して出力される)
