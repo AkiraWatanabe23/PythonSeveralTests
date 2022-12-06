1e1b34e5589e80ef32ffa55e4da5200f044ff968
Akira Watanabe <a0323w@gmail.com>
2022-12-06 10:57:17 +0900

    コンストラクタ「__init__」

--- SpecialMethods.py
+++ SpecialMethods.py
@@ -1,14 +1,14 @@
 #pythonの特殊メソッド各種について記述
 
 class Test:
-    #「__init__」pythonにおけるコンストラクタ
+    #「__init__」pythonにおけるコンストラクタ(データの初期化)
     #インスタンスを生成した時に実行される関数
     #「None」...この関数の戻り値の型(Noneの場合、戻り値なし)
     def __init__(self, name) -> None:
         self.name = name
 
     def out_put(self):
-        print(self.name, end="")
+        print(self.name)
 
 instance = Test("aaa")
-print(f"Your name is {instance.out_put()}, right?")
+instance.out_put()
