b13bf22e751f9ab10a36eb65d296b11a43f75a6a
Akira Watanabe <a0323w@gmail.com>
2022-12-06 11:46:12 +0900

    「__eq__」指定した2つのオブジェクトが等価かどうかを返す

--- SpecialMethods.py
+++ SpecialMethods.py
@@ -1,4 +1,5 @@
 #pythonの特殊メソッド各種について記述
+#https://blog.codecamp.jp/python-class-code
 
 class Test:
     #「__init__」pythonにおけるコンストラクタ(データの初期化)
@@ -7,8 +8,18 @@ class Test:
     def __init__(self, name) -> None:
         self.name = name
 
+    #「__eq__」条件分岐などで、「==」を利用した時に呼び出される
+    #return bool...2つのオブジェクトを比較し、同じならTrue, 違ったらFalseを返す
+    def __eq__(self, sample) -> bool:
+        return self.name == sample
+
     def out_put(self):
         print(self.name)
 
 instance = Test("aaa")
-instance.out_put()
+eq_test = Test("aaa")
+eq_test2 = Test("bbb")
+
+instance.out_put() # -> aaa
+print(instance == eq_test)  # -> True
+print(instance == eq_test2) # -> False
