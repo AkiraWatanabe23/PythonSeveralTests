ae47761fa88fdaa1f54fe3538debb749f7d0a65b
Akira Watanabe <a0323w@gmail.com>
2022-12-08 17:21:23 +0900

    インスタンス化の注意事項

--- SeveralTests.py
+++ SeveralTests.py
@@ -1,1 +1,29 @@
 #色々テストしたいことをここでやる
+
+#https://python.ms/class/
+
+class Animal:
+    att = "dog"
+
+    def honk(self):
+        print('bowwow')
+
+    def bark(self):
+        print('woof')
+
+instance = Animal() #インスタンス化
+instance.honk() # -> bowwow
+instance.bark() # -> woof
+
+"""
+※注意事項
+
+instance = Animal
+instance.honk()
+instance.bark()
+と記述した場合、コンパイル時にエラーが発生する
+(この時、コードを記述した時点ではエラーを起こさないため、注意!!)
+エラー↓
+TypeError: Animal.honk() missing 1 required positional argument: 'self'
+(honk()に必要な引数「self」がありません)
+"""
