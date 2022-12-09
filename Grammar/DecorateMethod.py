c017ee5643cfa0b88fe32b622298db549cb5bd61
Akira Watanabe <a0323w@gmail.com>
2022-12-09 13:11:08 +0900

    引数を受け取る関数デコレータ

--- Grammar/DecorateMethod.py
+++ Grammar/DecorateMethod.py
@@ -1,17 +1,18 @@
-#デコレーター関数...関数を修飾し、新しい関数を作成するもの(関数に付加機能を付けられる)
-#　　　　　　　　   複数の関数に対して同じ機能を付けたい時に使う
+#関数デコレータ...関数を修飾し、新しい関数を作成するもの(関数に付加機能を付けられる)
+#　　　　　　　　 複数の関数に対して同じ機能を付けたい時に使う
 #https://qiita.com/mtb_beta/items/d257519b018b8cd0cc2e
+#https://zenn.dev/ryo_kawamata/articles/learn_decorator_in_python
 
-#以下で関数デコレータを定義(関数デコレータ内で関数を定義し、処理を実行した後に関数をreturnする)
-#ex.1)引数なし
-def decorator(func_base): # <- func_base には、デコレートする関数(今回は「func()」)が引数に入る
+#以下で関数デコレータを定義(関数デコレータ内で関数を定義する)
+#ex.1)
+def decorator(func_base): # <- func_base には、デコレートする関数(今回は「func()」)が入る
     def in_decolate():
         print('check')
         func_base() # <- ここでデコレートする関数[func()]の処理を実行する
-    return in_decolate
+    return in_decolate # <- 処理を実行した後に関数を返す
 
 #デコレーターを指定
-#↓「decorator」というデコレーターの定義関数内で
+#↓「decorator」という関数デコレータ内で
 # 以下の関数(「func()」)をデコレート(装飾)する、という意味
 @decorator
 def func():
@@ -23,3 +24,25 @@ def func_test():
 
 func()      # -> check (\n) decorate
 func_test() # -> check (\n) test
+
+#ex.2)引数を受け取る関数デコレータ
+#def base(): ... デコレータの引数を受け取る
+def base(get): # <- get...関数デコレータの引数(「@...」の時に設定する)
+
+    #def child(): ... デコレートする関数を受け取る
+    def child(func): # <- func...デコレートする関数
+
+        #def wrap(): ... 実際の処理を記述する
+        def wrap(*args, **kwargs):
+            x = func(*args, **kwargs)
+            return f"<{get}> {x} </{get}>" # <- wrap()のreturn
+
+        return wrap # <- child()のreturn
+
+    return child # <- base()のreturn
+
+@base("h")
+def greet():
+    return 'Hello World'
+
+print(greet()) # -> <h> Hello World </h>
