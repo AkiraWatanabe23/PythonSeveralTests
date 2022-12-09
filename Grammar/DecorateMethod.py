66eb5fbb544d3bfcecf2636c538a26b7d82535a7
Akira Watanabe <a0323w@gmail.com>
2022-12-09 11:23:57 +0900

    コメント追加

--- Grammar/DecorateMethod.py
+++ Grammar/DecorateMethod.py
@@ -2,16 +2,24 @@
 #　　　　　　　　   複数の関数に対して同じ機能を付けたい時に使う
 #https://qiita.com/mtb_beta/items/d257519b018b8cd0cc2e
 
-#以下でデコレーター関数を定義(デコレーター関数内で関数を定義して使用する)
-def decorate(func_base): # <- func_base には、デコレートする関数(今回は「func()」)が引数に入る
+#以下で関数デコレータを定義(関数デコレータ内で関数を定義し、処理を実行した後に関数をreturnする)
+#ex.1)引数なし
+def decorator(func_base): # <- func_base には、デコレートする関数(今回は「func()」)が引数に入る
     def in_decolate():
         print('check')
-        func_base() # <- ここでデコレートする関数の処理を実行する
+        func_base() # <- ここでデコレートする関数[func()]の処理を実行する
     return in_decolate
 
 #デコレーターを指定
-@decorate
+#↓「decorator」というデコレーターの定義関数内で
+# 以下の関数(「func()」)をデコレート(装飾)する、という意味
+@decorator
 def func():
     print('decorate')
 
-func() # -> check (\n) decorate
+@decorator
+def func_test():
+    print("test")
+
+func()      # -> check (\n) decorate
+func_test() # -> check (\n) test
