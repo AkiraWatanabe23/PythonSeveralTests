e3e6d183f2eaed60b9c10575d7f93b4a86084b10
Akira Watanabe <a0323w@gmail.com>
2022-12-15 14:10:38 +0900

    ランダムな値を返す

--- Tests/Test.py
+++ Tests/Test.py
@@ -1,1 +1,22 @@
 #色々思いついたことをテストしてみる
+import random
+
+class Check:
+    order: int = 0
+    num: int = 0
+
+    def __init__(self, num: int):
+        self.num = num
+        self.order = random.randint(1, 100)
+
+def main():
+    instance: Check = Check(num=0)
+    instance.num = input()
+    
+    if instance.num == instance.order:
+        print("Congratulations!!")
+    else:
+        print(f"random number is {instance.order}")
+
+if __name__ == "__main__":
+    main()
