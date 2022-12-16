183ffed0ced9056267042cf2dcae17b98af34d14
Akira Watanabe <a0323w@gmail.com>
2022-12-16 15:05:25 +0900

    素数判定

--- Tests/PrimeNumber.py
+++ Tests/PrimeNumber.py
@@ -1,6 +1,7 @@
 #素数判定(入力値が素数かどうか判定する)
 
 def prime(n: int) -> bool:
+    
     for i in range(2, n):
         if n % i == 0:
             return False
@@ -15,3 +16,4 @@ if prime(num):
     print(f"{num} は素数です")
 else:
     print(f"{num} は素数ではありません")
+#現時点では正確な答えが出るが、ループ文に問題がある気がする
