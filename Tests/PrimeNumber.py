6d3e05f190d5193ee52649c018cea080772e0ae1
Akira Watanabe <a0323w@gmail.com>
2022-12-16 14:56:32 +0900

    素数判定

---
+++ Tests/PrimeNumber.py
@@ -0,0 +1,17 @@
+#素数判定(入力値が素数かどうか判定する)
+
+def prime(n: int) -> bool:
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+get = input("数字を1つ入力してください \n")
+num = int(get)
+
+if prime(num):
+    print(f"{num} は素数です")
+else:
+    print(f"{num} は素数ではありません")
