d486df4e06a9fc9a670f9d5eb1cdc6bd1eed3222
Akira Watanabe <a0323w@gmail.com>
2022-12-26 16:54:28 +0900

    2つの入力値の最大公約数を求める

--- SeveralTests/Numbers/EuclideanAlgorithm.py
+++ SeveralTests/Numbers/EuclideanAlgorithm.py
@@ -2,11 +2,20 @@
 #2つの自然数a, bにおいて、aをbで割った商をq、余りをrとしたとき、
 #「aとbの最大公約数」は「bとrの最大公約数」に等しい
 
-def gcd(n, m):
-    while m != 0:
-        n = m
-        m %= n
-        
-    return n
+def gcd(n, m): # GreatestCommonDivisor(最大公約数)
+    
+    if n >= m:
+        while m != 0:
+            n = m
+            m %= n
+        return n
+    elif n < m:
+        while n != 0:
+            m = n
+            n %= m
+        return m
 
-print(gcd(100, 30))
+get = input("自然数を2つ入力してください \n")
+nums = list(map(int, get.split()))
+
+print(gcd(nums[0], nums[1]))
