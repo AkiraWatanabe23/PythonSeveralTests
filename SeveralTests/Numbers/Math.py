5062063316f8d6070de6dc781127c867aeaa6b6c
Akira Watanabe <a0323w@gmail.com>
2023-01-20 10:41:00 +0900

    円周率PI

--- SeveralTests/Numbers/Math.py
+++ SeveralTests/Numbers/Math.py
@@ -1,4 +1,4 @@
-#ライブラリ「math」
+#モジュール「math」
 import math
 
 get = input("数字を入力してください \n")
@@ -7,11 +7,19 @@ num = int(get)
 
 #log(a, b) [return float] ... bを底としたaの対数(対数関数)
 #もし第二引数を省略した場合、ネイピア数eを底としたaの対数が返される
-print(math.log(num, 2))
+print(f"log2の{num}は、{math.log(num, 2)}です")
 
 #exp(a) [return float] ... ネイピア数eのa乗(指数関数)
 #※ math.exp(a) != math.e ** a (math.exp(a)の方がより正確な値を返す)
-print(math.exp(num))
+print(f"eの{num}乗は、{math.exp(num)} です")
+
+#math.pi ... 円周率PI(3.1415926535...)
+circumference = num * 2 * math.pi
+area = num ** 2 * math.pi
+
+print(f"円周率PI...{math.pi}")
+print(f"半径{num}の円の円周は{circumference}です")
+print(f"半径{num}の円の面積は{area}です")
 
 #math.floor ... 小数点以下切り捨て
 #math.ceil  ... 小数点以下切り上げ
