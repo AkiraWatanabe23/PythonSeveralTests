a1b12ac190239b82ad91a82b507714eb6d38113e
Akira Watanabe <a0323w@gmail.com>
2023-01-23 19:14:56 +0900

    命名規則確認中

--- SeveralTests/Numbers/euclidean_algorithm.py
+++ SeveralTests/Numbers/euclidean_algorithm.py
@@ -1,6 +1,6 @@
-'''ユークリッドの互除法
-   2つの自然数a, bにおいて、aをbで割った商をq、余りをrとしたとき、
-   「aとbの最大公約数」は「bとrの最大公約数」に等しい'''
+'''ユークリッドの互除法'''
+#2つの自然数a, bにおいて、aをbで割った商をq、余りをrとしたとき、
+#「aとbの最大公約数」は「bとrの最大公約数」に等しい
 
 def gcd(num1, num2):
     '''GreatestCommonDivisor(最大公約数)'''
@@ -9,13 +9,11 @@ def gcd(num1, num2):
             num1 = num2
             num2 %= num1
         return num1
-    elif num1 < num2:
+    else:
         while num1 != 0:
             num2 = num1
             num1 %= num2
         return num2
 
-get = input("自然数を2つ入力してください \n")
-a, b = map(int, get.split())
-
+a, b = map(int, input("自然数を2つ入力してください \n").split())
 print(gcd(a, b))
