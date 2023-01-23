f612a7b0be18f777fdda2c952ac1964ed9238f31
Akira Watanabe <a0323w@gmail.com>
2023-01-23 16:19:23 +0900

    pylintに沿った修正

--- SeveralTests/PylintTest.py
+++
@@ -1,7 +0,0 @@
-#「pylint(ツール)」についてのテスト
-
-nums: list = [0, 1, 2]
-words: tuple = ("aaa", "bbb", "ccc")
-
-print(type(nums))
-print(type(words))
---
+++ SeveralTests/pylint_test.py
@@ -0,0 +1,7 @@
+'''「pylint(ツール)」についてのテスト'''
+
+nums: list = [0, 1, 2]
+words: tuple = ("aaa", "bbb", "ccc")
+
+print(type(nums))  # -> <class 'list'>
+print(type(words)) # -> <class 'tuple'>
