6ad17806dcbaa76263501268ad5d81307f4c146b
Akira Watanabe <a0323w@gmail.com>
2023-02-02 16:38:17 +0900

    ログインのページ（tkinter）

--- SeveralTests/test_s.py
+++ SeveralTests/test_s.py
@@ -1,9 +1,31 @@
 '''テスト'''
+import tkinter as tk
 from instabot import Bot
 
+root = tk.Tk()
+root.title("Test App")
+root.geometry("500x500")
 bot = Bot()
 
-MY_USER_NAME = "user_name"
+name_label = tk.Label(text="ユーザー名を入れてください", foreground='#000000')
+name_label.pack()
+name_entry = tk.Entry(width=30)
+name_entry.pack()
 
-bot.login(username=MY_USER_NAME,
-          password="aaaaa")
+pass_name = tk.Label(text="パスワードを入れてください", foreground='#000000')
+pass_name.pack()
+pass_entry = tk.Entry(width=30)
+pass_entry.pack()
+
+def log_in(event):
+    '''ログインする'''
+    bot.login(username=name_entry.get(),
+              password=pass_entry.get())
+
+
+button = tk.Button(text="ログイン", width=20)
+button.bind("<Button-1>", log_in)
+button.pack()
+
+
+root.mainloop()
