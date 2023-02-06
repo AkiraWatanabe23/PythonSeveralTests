b8a9e6514538bbf2d15f4db6ec5f763f609c065a
Akira Watanabe <a0323w@gmail.com>
2023-02-06 15:40:26 +0900

    テキストの編集をしたい

--- GameTest/data_test.py
+++ GameTest/data_test.py
@@ -12,9 +12,10 @@ for k, v in files.items():
     try:
         with open(str(v), 'r', encoding='UTF-8') as file:
             data[k] = file.read()
+        # with open(str(v), mode='a', encoding='UTF-8') as f:
+        #     data[k] = f.write("\n追記")
     except FileNotFoundError:
         print("指定したファイルが見つかりませんでした")
-
 
 for k in data.items():
     print(f"[{k}]")
